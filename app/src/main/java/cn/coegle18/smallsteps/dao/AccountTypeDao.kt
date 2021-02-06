a43d7db531861452e4d9b5edf7011e1b38c6b695
杨佳雨 <coegle18@gmail.com>
2021-02-06 22:34:38 +0800

    建立了 AccountTypeDao 类

---
+++ app/src/main/java/cn/coegle18/smallsteps/dao/AccountTypeDao.kt
@@ -0,0 +1,25 @@
+package cn.coegle18.smallsteps.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Transaction
+import cn.coegle18.smallsteps.PrimaryAccountType
+import cn.coegle18.smallsteps.entity.AccountOfAccountType
+import cn.coegle18.smallsteps.entity.AccountType
+
+
+@Dao
+interface AccountTypeDao {
+    // 查询某 PrimaryAccountType 下的账户实例
+    @Transaction
+    @Query("Select * from AccountType where primaryAccountType = :primaryAccountType")
+    fun queryPrimaryAccountTypeInstance(primaryAccountType: PrimaryAccountType): AccountOfAccountType
+
+    // 查询某 PrimaryAccountType 下的可创建的账户类型
+    @Query("Select * from AccountType where primaryAccountType = :primaryAccountType")
+    fun queryPrimaryAccountType(primaryAccountType: PrimaryAccountType): List<AccountType>
+
+    // 查询某最终账户类型
+    @Query("Select * from AccountType where parentId = :pId")
+    fun queryFinalAccountType(pId: Int): List<AccountType>
+}
