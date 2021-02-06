8286cfb7ad50d197289fa639ac0ad95bf7b70fdb
杨佳雨 <coegle18@gmail.com>
2021-02-06 22:04:23 +0800

    建立了 Category 和 Bill 之间的一对多关系

---
+++ app/src/main/java/cn/coegle18/smallsteps/entity/BillOfCategory.kt
@@ -0,0 +1,13 @@
+package cn.coegle18.smallsteps.entity
+
+import androidx.room.Embedded
+import androidx.room.Relation
+
+data class BillOfCategory(
+        @Embedded val category: Category,
+        @Relation(
+                parentColumn = "categoryId",
+                entityColumn = "category"
+        )
+        val accounts: List<Account>
+)
