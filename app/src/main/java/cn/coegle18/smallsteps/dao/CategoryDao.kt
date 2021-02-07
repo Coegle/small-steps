9c6f7aa5ab0d9c081f5a25f4e57e797eab6ca82b
杨佳雨 <coegle18@gmail.com>
2021-02-07 16:39:58 +0800

    添加了： 1.查询指定子分类的账单的方法； 2. 查询指定母分类的账单的方法

--- app/src/main/java/cn/coegle18/smallsteps/dao/CategoryDao.kt
+++ app/src/main/java/cn/coegle18/smallsteps/dao/CategoryDao.kt
@@ -1,10 +1,9 @@
 package cn.coegle18.smallsteps.dao
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
+import androidx.room.*
 import cn.coegle18.smallsteps.Visible
+import cn.coegle18.smallsteps.entity.Bill
+import cn.coegle18.smallsteps.entity.BillOfCategory
 import cn.coegle18.smallsteps.entity.Category
 
 @Dao
@@ -19,7 +18,22 @@ interface CategoryDao {
     @Query("Select * from Category where visible = :visible")
     fun queryCategory(visible: Visible): List<Category>
 
-    // 查询指定分类的子分类（0 即查询母分类）
+    // 查询指定分类的子分类（0 即查询所有母分类）
     @Query("Select * from Category where parentId = :parentId")
-    fun querySubCategory(parentId: Int): List<Category>
+    fun querySubCategory(parentId: Long): List<Category>
+
+    // 查询指定子分类的账单
+    @Transaction
+    @Query("Select * from Category where categoryId = :category")
+    fun queryBillsOfSubCategory(category: Long): BillOfCategory
+
+    // 查询指定母分类的账单
+    fun queryBillsOfCategory(parentCategory: Long): List<Bill> {
+        val categoryList = querySubCategory(parentCategory)
+        val bills = emptyList<Bill>().toMutableList()
+        for (category in categoryList) {
+            bills += queryBillsOfSubCategory(category.categoryId).bills
+        }
+        return bills
+    }
 }
